81791a14476fa42edda802871d8464226dfb74d2
Sahareior Sijan <sahareiors@gmail.com>
2024-08-19 23:15:08 +0600

    updated

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require('express')
 const cors = require('cors')
 const SSLCommerzPayment = require('sslcommerz-lts')
 const app = express()
-app.use(cors())
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Allow credentials
+};
+app.use(cors(corsOptions))
 app.use(express.json());
 const port = process.env.PORT || 5000; 
 
 const store_id = 'testc64df3c78f3bc0'
 const store_passwd = 'testc64df3c78f3bc0@ssl'
 const is_live = false //true for live, false for sandbox
-// shopping-store  wiettPtPC5wEEoxm
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://shopping-store:wiettPtPC5wEEoxm@cluster0.j0rll9s.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// gChfk7GCRUt3OqmA   kalles
+
+// const uri = "mongodb://localhost:27017"
+const uri = "mongodb+srv://kalles:gChfk7GCRUt3OqmA@cluster0.s4ykc77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,24 +32,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    await client.connect();
     const productsCollections = client.db("Cloth-Store").collection("All-Products");
-    const orderCollections = client.db("Cloth-Store").collection("Orders")
-    // Send a ping to confirm a successful connection
+    const orderCollections = client.db("Cloth-Store").collection("Orders");
+
     app.post('/payments', async (req, res) => {
-      const body = req.body
-     const items = req.body
-     const phone = req.body.phone
-     const trans_id= new ObjectId().toString()
+      const { body } = req;
+      const items = body;
+      const { phone } = body;
+      const trans_id = new ObjectId().toString();
       const data = {
         total_amount: body.total,
         currency: 'BDT',
-        tran_id: trans_id, // use unique tran_id for each api call
-        success_url: ` https://app-flame-five.vercel.app/payments/success/${trans_id}`,
-        fail_url: 'http://localhost:3030/fail',
-        cancel_url: 'http://localhost:3030/cancel',
-        ipn_url: 'http://localhost:3030/ipn',
+        tran_id: trans_id,
+        success_url: `http://localhost:5000/payments/success/${trans_id}`,
+        fail_url: 'http://localhost:5173/fail',
+        cancel_url: 'http://localhost:5173/cancel',
+        ipn_url: 'http://localhost:5173/ipn',
         shipping_method: 'Courier',
         product_name: 'Computer.',
         product_category: 'Electronic',
@@ -64,174 +70,126 @@ async function run() {
         ship_state: 'Dhaka',
         ship_postcode: 1000,
         ship_country: 'Bangladesh',
-    };
+      };
     
-      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-      sslcz.init(data).then(apiResponse => {
-          // Redirect the user to payment gateway
-          let GatewayPageURL = apiResponse.GatewayPageURL
-          res.send({url:GatewayPageURL})
-          const date = new Date()
-          const day = date.toLocaleString()
-          const day_date = date.toDateString()
-          
-// lll
-          const paymentData = {
-            data,
-            transection_id : trans_id,
-            paid: false,
-            orderdItem: items,
-            orderdTime: day,
-            orderDate: day_date
-          }
-console.log(paymentData)
-          const result =  orderCollections.insertOne(paymentData)
-          
-      });
-      
-  })
-
-  app.post('/payments/success/:transID', async (req, res) => {
-    const trans_id = req.params.transID;
-    const result = await orderCollections.updateOne(
-  { transection_id: trans_id},
-  {
-    $set:{paid:true}
-  }
-)
-
-if(result.modifiedCount>0){
-  res.redirect('https://mellifluous-starship-c61165.netlify.app/payments/success')
-}
-
-
-  });
-
-
-  app.get('/orders', async (req, res) => {
-    const { date } = req.query;
-  console.log(req.query)
-    let query = {};
-    if (date) {
-      query.orderDate = date;
-    }
-  
-    const cursor = orderCollections.find(query);
-
-    // Sort the results in ascending order by the 'orderdTime' field
-    cursor.sort({ orderdTime: -1 });
-  
-    const result = await cursor.toArray();
-  
-    res.send(result);
-  });
-  
-  app.get('/allproducts', async (req, res) => {
-    const { size, color, brand, category,gender } = req.query;
-   
-    let query = {}; // Initial query object
-  
-    if (size) {
-      query.size = size;
-    }
-  
-    if (color) {
-      query.color = color;
-    }
-  
-    if (brand) {
-      query.brand = brand;
-    }
-    if(category){
-      query.category = category
-    }
-    if(gender){
-      query.gender = gender
-    }
-    console.log(query)
-    try {
-      // Use your Product model to fetch data based on the query
-      // Replace "Product" with your actual MongoDB model
-      const products = await productsCollections.find(query).toArray();
-      
-      // Return the filtered products as a response
-      res.send(products);
-  
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then(async apiResponse => {
+        const GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
     
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while fetching products.' });
-    }
-  });
-  
-  
-    app.get('/products', async (req,res)=>{
-        const cursor = productsCollections.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-    app.get('/products/:id', async(req,res)=>{
-        const id = req.params.id 
-        const query = {_id: new ObjectId(id)}
-        const cursor = productsCollections.find(query)
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-
-    app.delete('/products/:id', async(req,res)=>{
-        const id = req.params.id 
-      
-        const query = {_id: new ObjectId(id)}
-        const result = await productsCollections.deleteOne(query)
-        res.send(result)
-    })
-
-    app.patch('/products/:id', async(req,res)=>{
-      const id = req.params.id 
-      const data = req.body
-      const querry = {_id: new ObjectId(id)}
-      console.log(data)
-      let updateData
-      if(data.name == 'highlighted'){
-       updateData = {
-        $set: {
-          highlighted: data.data,
+        const date = new Date();
+        const day = date.toLocaleString();
+        const day_date = date.toDateString();
+    
+        const paymentData = {
+          data,
+          transection_id: trans_id,
+          paid: false,
+          orderdItem: items,
+          orderdTime: day,
+          orderDate: day_date,
+        };
+    
+        try {
+          const result = await orderCollections.insertOne(paymentData);
+        } catch (error) {
+          console.error('Error inserting payment data:', error);
         }
-       }
+      }).catch(error => {
+        console.error('Error initializing SSLCommerz:', error);
+        res.status(500).send('An error occurred');
+      });
+    });
+
+    app.post('/payments/success/:transID', async (req, res) => {
+      console.log('Payment success endpoint hit');
+      const trans_id = req.params.transID;
+      const result = await orderCollections.updateOne(
+        { transection_id: trans_id },
+        { $set: { paid: true } }
+      );
+      console.log(trans_id, result);
+
+      if (result.modifiedCount > 0) {
+        res.redirect(`http://localhost:5173/payments/success/${trans_id}`);
+      } else {
+        res.status(404).send('Transaction not found or not updated');
       }
-      else if(data.name =='hotdeals'){
-        updateData = {
-          $set: {
-            hotDeals: data.data,
-          }
-         }
-      }
-      else if (data.name == 'newly'){
-        updateData = {
-          $set: {
-            newlyArrived: data.data,
-          }
-         }
-      }
-      else if (data.name == 'bestsells'){
-        updateData = {
-          $set: {
-            bestSells: data.data,
-          }
-         }
+    });
+
+    app.get('/orders', async (req, res) => {
+      const { date } = req.query;
+      let query = {};
+      if (date) {
+        query.orderDate = date;
       }
     
-      const result = await productsCollections.updateOne(querry,updateData)
-      res.send(result)
-   
-    })
+      const cursor = orderCollections.find(query);
+      cursor.sort({ orderdTime: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  
+    app.get('/allproducts', async (req, res) => {
+      const { size, color, brand, category, gender } = req.query;
+      let query = {};
+  
+      if (size) query.size = size;
+      if (color) query.color = color;
+      if (brand) query.brand = brand;
+      if (category) query.category = category;
+      if (gender) query.gender = gender;
 
-    app.put('/products/:id', async (req,res)=>{
-      const id = req.params.id 
-      const data = req.body 
-      const query = {_id: new ObjectId(id)}
-      const updatedData ={
-        $set:{
+      try {
+        const products = await productsCollections.find(query).toArray();
+        res.send(products);
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching products.' });
+      }
+    });
+  
+    app.get('/products', async (req, res) => {
+      const cursor = productsCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = productsCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const [key, value] = Object.entries(data)[0];
+      const updateData = { $set: { [key]: value } };
+  
+      try {
+        const result = await productsCollections.updateOne(query, updateData);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: 'An error occurred while updating the product' });
+      }
+    });
+
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedData = {
+        $set: {
           price: data.price,
           category: data.category,
           email: data.email,
@@ -240,18 +198,16 @@ if(result.modifiedCount>0){
           zoomImage: data.zoomImage,
           color: data.color,
           gender: data.gender,
-          title: data.title
+          title: data.title,
         }
-      }
-      const result = await productsCollections.updateOne(query,updatedData)
-      res.send(result)
-    })
+      };
+      const result = await productsCollections.updateOne(query, updatedData);
+      res.send(result);
+    });
 
-    app.post('/products', async(req,res)=>{
-      const data = req.body
-     const newData ={
-      
-       
+    app.post('/products', async (req, res) => {
+      const data = req.body;
+      const newData = {
         gender: data.gender,
         category: data.category,
         title: data.title,
@@ -260,25 +216,23 @@ if(result.modifiedCount>0){
         newlyArrived: "yes",
         highlighted: "no",
         price: data.price,
-        size:data.Psize,
-        brand:data.PBrand,
+        size: data.Psize,
+        brand: data.PBrand,
         img: data.img,
         img2: data.img2,
         zoomImage: data.zoomImage,
         date: data.date,
         color: data.color,
-        id: data.id
-     }
-     const result = await productsCollections.insertOne(newData);
-     res.send(result);
-  
-    })
- 
-    app.post('/orders',async (req,res)=>{
-      const data = req.body
-      console.log(data)
-    })
+        id: data.id,
+      };
+      const result = await productsCollections.insertOne(newData);
+      res.send(result);
+    });
 
+    app.post('/orders', async (req, res) => {
+      const data = req.body;
+      console.log(data);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -287,12 +241,12 @@ if(result.modifiedCount>0){
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-app.get('/', (req,res)=> {
-    res.send('Hello Pithibi')
-})
+app.get('/', (req, res) => {
+  res.send('Hello Pithibi');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
